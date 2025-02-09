4b9f653f856c7217545a72975dfef19297c5372b
juanpablor <juanpablor@users.noreply.github.com>
2025-02-09 16:42:56 +0000

    deploy: juanpablor/template@9389c9a05e34114db8130c9b370c87751f652233

---
+++ 6eb5140f-1b0a358310c289b55e16.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktemplate=self.webpackChunktemplate||[]).push([[310],{658:function(t,h,c){c.d(h,{RJC:function(){return v}});var e=c(2225);function v(t){return(0,e.k5)({tag:"svg",attr:{version:"1.1",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M14.5 0h-13c-0.825 0-1.5 0.675-1.5 1.5v13c0 0.825 0.675 1.5 1.5 1.5h6.5v-7h-2v-2h2v-1c0-1.653 1.347-3 3-3h2v2h-2c-0.55 0-1 0.45-1 1v1h3l-0.5 2h-2.5v7h4.5c0.825 0 1.5-0.675 1.5-1.5v-13c0-0.825-0.675-1.5-1.5-1.5z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=6eb5140f-1b0a358310c289b55e16.js.map
